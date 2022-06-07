df5dab6c5cd0307b1401ca263c12d0f2e7a5d857
Jino Hong <jinohong1993@gmail.com>
2022-06-07 16:17:18 -0300

    lab cronometro

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,70 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      // this.currentTime++;
+      // let starttimer = this.currentTime++;
+      // return setTime(starttimer);     
+      
+      return this.currentTime++
+    },1000);
+
+    // this.currentTime = setInterval((setStart) => {
+    //   return this.intervalId++;      
+    // },1000)
+   
+    
+    // return printTimeCallback();
   }
 
   getMinutes() {
     // ... your code goes here
+    let minutes = Math.floor(this.currentTime/60);
+    return minutes;
   }
 
   getSeconds() {
     // ... your code goes here
+    let seconds = Math.floor(this.currentTime);
+    if (seconds >= 60) {
+      return seconds%60;
+    }
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+  //  let twodigit = this.currentTime;
+  //   if ((String(value)).length = 1 ) {
+  //     return `0${value}`;
+  //   }
+  //  else return String(value);
+
+    if (value < 10) {
+      let compute = `0`+value;
+      return String(compute);
+    }else return String(value);
   }
 
   stop() {
     // ... your code goes here
+    return clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes())+":"+this.computeTwoDigitNumber(this.getSeconds());        
   }
 }
 
